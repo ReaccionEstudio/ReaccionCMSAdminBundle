ca9eb59c79711d93b95dc7a8703c5dc1ae29a788
viher3 <viher_3@hotmail.com>
2018-09-12 00:56:57 +0200

    IndexController created

---
+++ Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller;
+
+	use Symfony\Component\HttpFoundation\JsonResponse;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+	class IndexController extends Controller
+	{
+		public function index()
+		{
+			return $this->render("@ReaccionCMSAdminBundle/login.html.twig");
+		}
+	}
