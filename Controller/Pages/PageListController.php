c08384284c6573a90ac867721af93d12394816ad
viher3 <viher_3@hotmail.com>
2018-09-13 17:52:05 +0200

    Get all pages from db

--- Controller/Pages/PageListController.php
+++ Controller/Pages/PageListController.php
@@ -3,11 +3,19 @@
 	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Pages;
 
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
 
 	class PageListController extends Controller
 	{
 		public function index()
 		{
-			return $this->render("@ReaccionCMSAdminBundle/pages/list.html.twig");
+			$em = $this->getDoctrine()->getManager();
+			$pages = $em->getRepository(Page::class)->findAll();
+
+			return $this->render("@ReaccionCMSAdminBundle/pages/list.html.twig",
+				[
+					'pages' => $pages
+				]
+			);
 		}
 	}
