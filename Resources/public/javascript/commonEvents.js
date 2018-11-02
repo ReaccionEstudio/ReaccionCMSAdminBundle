91676ac010edbf560b57d981504301a1e4831b8c
viher3 <viher_3@hotmail.com>
2018-11-02 15:10:07 +0100

    Admin panel assets

---
+++ Resources/public/javascript/commonEvents.js
@@ -0,0 +1,12 @@
+/**
+ * App common events.
+ *
+ * @author 	Alberto Vian - info@reaccionestudio.com
+ * @website reaccionestudio.com
+ */
+
+ $(document).ready(function()
+ {
+ 	// Bootstrap tooltips events
+  	$('[data-toggle="tooltip"]').tooltip();
+ });
