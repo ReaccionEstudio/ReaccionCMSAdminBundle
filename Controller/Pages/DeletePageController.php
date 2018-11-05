ae69398117986197483ed6348c0b1967472afb97
viher3 <viher_3@hotmail.com>
2018-11-06 00:28:07 +0100

    Created DeletePageController

---
+++ Controller/Pages/DeletePageController.php
@@ -0,0 +1,45 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Pages;
+
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Request;
+
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
+	use App\ReaccionEstudio\ReaccionCMSAdminBundle\Form\Pages\PageType;
+	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+	class DeletePageController extends Controller
+	{
+		public function index(Page $page)
+		{
+			$em = $this->getDoctrine()->getManager();
+
+			if(empty($page))
+			{
+				throw new NotFoundHttpException("Page content not found");
+			}
+
+			$pageName = $page->getName();
+
+			try
+			{
+				// remove
+				$em->remove($page);
+				$em->flush();
+
+				$this->addFlash('success', 'Page <strong>' . $pageName . '</strong> was removed correctly.');
+
+				return $this->redirectToRoute('reaccion_cms_admin_pages_index');
+			}
+			catch(\Exception $e)
+			{
+				$this->addFlash('error', 'Error removing <strong>' . $pageName . '</strong> content: ' . $e->getMessage() . '.');
+
+				return 	$this->redirectToRoute(
+							'reaccion_cms_admin_pages_detail', 
+							array('page' => $page->getId())
+						);
+			}
+		}
+	}
