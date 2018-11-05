58ffb5ba2360500b8f3a17a2144709eb58a736a5
viher3 <viher_3@hotmail.com>
2018-11-05 23:16:07 +0100

    PageForm and PageSeoForm submit

--- Controller/Pages/PageDetailController.php
+++ Controller/Pages/PageDetailController.php
@@ -12,11 +12,37 @@
 
 	class PageDetailController extends Controller
 	{
-		public function index(Page $page)
+		public function index(Page $page, Request $request)
 		{
+			$em = $this->getDoctrine()->getManager();
+
 			// forms
 			$pageForm = $this->createForm(PageType::class, $page);
 			$seoPageForm = $this->createForm(SeoPageType::class, $page);
+
+			// handle forms request
+			$pageForm->handleRequest($request);
+			$seoPageForm->handleRequest($request);
+
+			// update page entity for both forms submitted data
+			if( 
+				( $pageForm->isSubmitted() && $pageForm->isValid() ) || 
+				( $seoPageForm->isSubmitted() && $seoPageForm->isValid() ) 
+			) 
+			{
+				try
+				{
+					$em->persist($page);
+					$em->flush();
+
+					$this->addFlash('success', 'Page has been updated correctly.');
+
+				}
+				catch(\Exception $e)
+				{
+					$this->addFlash('error', 'Error updating page: ' . $e->getMessage() . '.');
+				}
+			}
 
 			return $this->render("@ReaccionCMSAdminBundle/pages/detail.html.twig",
 				[
