ef6abd7a1cc06c7f3ead379980a773fe8d99e1aa
viher3 <viher_3@hotmail.com>
2018-11-08 14:01:07 +0100

    Configuration list view

---
+++ Controller/Configuration/ConfigurationListController.php
@@ -0,0 +1,33 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Configuration;
+
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Configuration;
+
+	class ConfigurationListController extends Controller
+	{
+		public function index(Request $request)
+		{
+			$em = $this->getDoctrine()->getManager();
+			$config = $em->getRepository(Configuration::class)->findBy(
+						array(),
+						array('id' => 'DESC')
+					 );
+
+			// pagination
+			$paginator = $this->get('knp_paginator');
+		    $config = $paginator->paginate(
+		        $config,
+		        $request->query->getInt('page', 1),
+		        $this->getParameter("pagination_page_limit")
+		    );
+
+			return $this->render("@ReaccionCMSAdminBundle/configuration/list.html.twig",
+				[
+					'config' => $config
+				]
+			);
+		}
+	}
