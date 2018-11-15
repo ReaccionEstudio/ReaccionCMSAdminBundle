b65dd78d7addd315da12a31216a198ccb029781a
viher3 <viher_3@hotmail.com>
2018-11-15 02:22:35 +0100

    Return media entity as json

--- Controller/Media/MediaDetailController.php
+++ Controller/Media/MediaDetailController.php
@@ -3,6 +3,7 @@
 	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Media;
 
 	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 	use Symfony\Component\Translation\TranslatorInterface;
 	use App\ReaccionEstudio\ReaccionCMSAdminBundle\Form\Media\MediaType;
@@ -10,9 +11,26 @@
 
 	class MediaDetailController extends Controller
 	{
-		public function index(Media $media)
+		public function index(Media $media, Request $request)
 		{
-			
+			if($request->query->get('json'))
+			{
+				$response = [
+					'id' => $media->getId(),
+					'name' => $media->getName(),
+					'path' => $media->getPath(),
+					'largePath' => $media->getLargePath(),
+					'mediumPath' => $media->getMediumPath(),
+					'smallPath' => $media->getSmallPath(),
+					'size' => $media->getSize(),
+					'largeSize' => $media->getLargeSize(),
+					'mediumSize' => $media->getMediumSize(),
+					'smallSize' => $media->getSmallSize(),
+					'mimeType' => $media->getMimeType()
+				];
+				
+				return new JsonResponse($response);
+			}
 
 			return $this->render("@ReaccionCMSAdminBundle/media/detail.html.twig",
 				[
