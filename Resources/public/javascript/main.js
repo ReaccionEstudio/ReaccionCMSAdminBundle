5f9bf1e1c92d286a90fe69b2f7b540c78bc3f466
viher3 <viher_3@hotmail.com>
2018-11-15 12:10:06 +0100

    Added pageContentAddRegex in main.js

--- Resources/public/javascript/main.js
+++ Resources/public/javascript/main.js
@@ -14,13 +14,15 @@ const currentRoute 	= window.location.pathname;
 
 $(document).ready(function()
 {
+	let pageContentAddRegex = /admin\/pages\/\d\/content\/add/gi;
+	
 	if(currentRoute == "/admin/media/add")
 	{
 		let media = new Media();
 			media.formEvents();
 	}
 
-	if(currentRoute == "/admin/pages/2/content/add") // TODO: regex
+	if(pageContentAddRegex.test(currentRoute))
 	{
 		let page = new Page();
 			page.formEvents();
