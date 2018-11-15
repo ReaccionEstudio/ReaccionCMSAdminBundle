3699ac2bc293497233c3702c319e542fadf9d2a7
viher3 <viher_3@hotmail.com>
2018-11-15 14:44:28 +0100

    Created findByPath() method in MediaDetailCtrl

--- Controller/Media/MediaDetailController.php
+++ Controller/Media/MediaDetailController.php
@@ -11,11 +11,72 @@
 
 	class MediaDetailController extends Controller
 	{
+		/**
+		 * List all media entities
+		 *
+		 * @param  Media 				$media 		Media entity
+		 * @param  Request 				$request 	Request object
+		 * @return TwigView | JSON 		[type]		Return a Twig view or a JSON response
+		 */
 		public function index(Media $media, Request $request)
 		{
-			if($request->query->get('json'))
+			if($request->query->get('json')) return $this->getJsonResponse($media);
+
+			return $this->render("@ReaccionCMSAdminBundle/media/detail.html.twig",
+				[
+					'media' => $media
+				]
+			);
+		}
+
+		/**
+		 * Find Media entity by any media path
+		 *
+		 * @param  Request 			$request 	Request object
+		 * @return JsonResponse 	[type] 		JSON object
+		 */
+		public function findByPath(Request $request)
+		{
+			$path = $request->request->get('path');
+
+			if(empty($path))
 			{
-				$response = [
+				return new JsonResponse([ 'error' => 'Empty request parameters' ]);
+			}
+
+			try
+			{
+				$dql = "SELECT m 
+						FROM  App\ReaccionEstudio\ReaccionCMSBundle\Entity\Media m 
+						WHERE m.path = :mediaPath 
+						OR m.large_path = :mediaPath 
+						OR m.medium_path = :mediaPath 
+						OR m.small_path = :mediaPath 
+						";
+
+				$em = $this->getDoctrine()->getManager();
+				$query = $em->createQuery($dql)
+				   			->setParameter("mediaPath", $path);
+
+				$result = $query->getSingleResult();
+
+				return $this->getJsonResponse($result);
+			}
+			catch(\Exception $e)
+			{
+				return new JsonResponse(['error' => $e->getMessage()]);
+			}
+		}
+
+		/**
+		 * Return a media object response as JSON
+		 *
+		 * @param Media 			$media 		Media entity
+		 * @param JsonResponse 		[type] 		JSON
+		 */
+		private function getJsonResponse(Media $media)
+		{
+			$response = [
 					'id' => $media->getId(),
 					'name' => $media->getName(),
 					'path' => $media->getPath(),
@@ -29,13 +90,6 @@
 					'mimeType' => $media->getMimeType()
 				];
 				
-				return new JsonResponse($response);
-			}
-
-			return $this->render("@ReaccionCMSAdminBundle/media/detail.html.twig",
-				[
-					'media' => $media
-				]
-			);
+			return new JsonResponse($response);
 		}
 	}
