ae3174df799acea65687f13aab585905ec38e3c7
viher3 <viher_3@hotmail.com>
2018-11-18 22:19:31 +0100

    Languages Constants

---
+++ Constants/Languages.php
@@ -0,0 +1,19 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Constants;
+
+	/**
+	 * Languages constants
+	 */
+	class Languages
+	{
+		const LANGUAGES = [
+			'en', 
+			'es'
+		];
+
+		const LANGUAGE_ICONS = [
+			'en' => 'gb', 
+			'es' => 'es'
+		];
+	}
