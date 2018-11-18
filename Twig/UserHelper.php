10d983f5a9552a5f06220980e774cba443dd3f3f
viher3 <viher_3@hotmail.com>
2018-11-19 00:20:22 +0100

    Get roles translation key

---
+++ Twig/UserHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Twig;
+
+use Services\Managers\ManagerPermissions;
+use App\ReaccionEstudio\ReaccionCMSAdminBundle\Constants\Languages;
+
+/**
+ * UserHelper class (Twig_Extension)
+ *
+ * @author Alberto Vian <alberto@reaccionestudio.com>
+ */
+class UserHelper extends \Twig_Extension
+{
+	public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('getUserNickname', array($this, 'getUserNickname')),
+            new \Twig_SimpleFunction('getUserMainRoleTranslationKey', array($this, 'getUserMainRoleTranslationKey'))
+        );
+    }
+
+    /**
+     * Get user nickname if exists
+     *
+     * @param   User    $user     User Twig object
+     * @return  String  [type]    User nickname | Username
+     */
+    public function getUserNickname($user)
+    {
+        if($user->getNickname()) 
+        {
+            return $user->getNickname();
+        }
+
+        return $user->getUsername();
+    }
+
+    /**
+     * Get user main role translation key
+     *
+     * @param   User    $user     User Twig object
+     * @return  String  [type]    User main role
+     */
+    public function getUserMainRoleTranslationKey($user)
+    {
+        $roles = $user->getRoles();
+
+        if(empty($roles[0])) return '';
+        return "roles." . strtolower($roles[0]);
+    }
+
+	public function getName()
+    {
+        return 'UserHelper';
+    }
+}
