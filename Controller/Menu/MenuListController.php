9b8a9fa419f9407f240ff4b87518d066b46e86c9
viher3 <viher_3@hotmail.com>
2018-11-24 01:09:06 +0100

    Created MenuListController

---
+++ Controller/Menu/MenuListController.php
@@ -0,0 +1,38 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Menu;
+
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\Translation\TranslatorInterface;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Menu;
+
+	use App\ReaccionEstudio\ReaccionCMSAdminBundle\Services\Menu\MenuService;
+
+	class MenuListController extends Controller
+	{
+		public function index(Request $request, TranslatorInterface $translator)
+		{
+			$em = $this->getDoctrine()->getManager();
+
+			$dql =  "
+					SELECT 
+					m.id, p.id AS parent_id, m.name, m.type, m.target, m.position
+					FROM  App\ReaccionEstudio\ReaccionCMSBundle\Entity\Menu m 
+					LEFT JOIN App\ReaccionEstudio\ReaccionCMSBundle\Entity\Menu p 
+					WITH p.id = m.parent
+					";
+
+			$query = $em->createQuery($dql);
+			$menu = $query->getArrayResult();
+
+			$menuService = new MenuService();
+			$nested = $menuService->buildNestedArray($menu);
+
+			return $this->render("@ReaccionCMSAdminBundle/menu/list.html.twig",
+				[
+					'menu' => $nested
+				]
+			);
+		}
+	}
