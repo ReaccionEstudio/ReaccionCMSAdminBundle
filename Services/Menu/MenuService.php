eee8b18f656d295ec6056309a43048e64a54cbfa
viher3 <viher_3@hotmail.com>
2018-11-27 16:25:41 +0100

    Added getNextItemPosition() method in MenuService

--- Services/Menu/MenuService.php
+++ Services/Menu/MenuService.php
@@ -2,6 +2,9 @@
 
 	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Services\Menu;
 
+	use Doctrine\ORM\EntityManager;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Menu;
+
 	/**
 	 * Menu service
 	 *
@@ -9,6 +12,21 @@
 	 */
 	class MenuService
 	{
+		/**
+		 * @var EntityManagerInterface
+		 *
+		 * EntityManager
+		 */
+		private $em;
+
+		/**
+		 * Constructor
+		 */
+		public function __construct(EntityManager $em)
+		{
+			$this->em = $em;
+		}
+
 		/**
 		 * Build nested array with menu items
 		 *
@@ -53,5 +71,34 @@
 			}
 
 			return $nested;
+		}
+
+		/**
+		 * Get next item position value
+		 *
+		 * @param  Integer 	$parentId 	Parent menu item id
+		 * @return Integer 	[type] 		Next item position
+		 */
+		public function getNextItemPosition(Int $parentId) : Int
+		{
+			$dql =  "SELECT (MAX(m.position) + 1) AS next_position 
+					 FROM  App\ReaccionEstudio\ReaccionCMSBundle\Entity\Menu m 
+					";
+
+			if($parentId > 0)
+			{
+				$dql .= "WHERE m.parent = :parent";
+			}
+
+			$query = $this->em->createQuery($dql);
+
+			if($parentId > 0)
+			{
+				$query->setParameter('parent', $parentId);
+			}
+
+			$result = $query->getSingleResult();
+
+			return (isset($result['next_position'])) ? $result['next_position'] : 1;
 		}
 	}
