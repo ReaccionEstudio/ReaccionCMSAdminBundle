a6a265ea8f8c7dc09ed56d2df04e2e9fc5258931
viher3 <viher_3@hotmail.com>
2018-11-27 16:27:02 +0100

    Create menu item form

--- Controller/Menu/MenuCreateController.php
+++ Controller/Menu/MenuCreateController.php
@@ -21,7 +21,55 @@
 
 			if ($form->isSubmitted() && $form->isValid()) 
 			{
-				
+				$em = $this->getDoctrine()->getManager();
+
+				try
+				{
+					// set Value
+					if(($form['type']->getData() == "url") )
+					{
+						$menuValue = $form['urlValue']->getData();
+					}
+					else if($form['type']->getData() == "page")
+					{
+						$page = $form['pageValue']->getData();
+						$menuValue = $page->getId();
+					}
+
+					$menu->setValue($menuValue);
+
+					// set next position
+					$nextPosition = $this->get("reaccion_cms_admin.menu")->getNextItemPosition($parent);
+					$menu->setPosition($nextPosition);
+
+					if($parent)
+					{
+						$parentMenu = $em->getRepository(Menu::class)->findOneBy(['id' => $parent]);
+						
+						if($parentMenu) 
+						{
+							$menu->setParent($parentMenu);
+						}
+					}
+
+					// save
+					$em->persist($menu);
+					$em->flush();
+
+					$this->addFlash('success', $translator->trans('menu_form.create_success_message') );
+					return $this->redirectToRoute('reaccion_cms_admin_preferences_menu');
+				}
+				catch(\Exception $e)
+				{
+					$errMssg =  $translator->trans(
+									"menu_form.create_error_message", 
+									array(
+										'%name%' => $form['name']->getData(),
+										'%error%' => $e->getMessage()
+									) 
+								);
+					$this->addFlash('error', $errMssg);
+				}
 			}
 
 			return $this->render("@ReaccionCMSAdminBundle/menu/form.html.twig",
