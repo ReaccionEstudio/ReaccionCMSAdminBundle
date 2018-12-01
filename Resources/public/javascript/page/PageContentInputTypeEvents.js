2c0a1673e5c23dccaf5a2aa52a742e321378cebd
viher3 <viher_3@hotmail.com>
2018-12-01 17:15:25 +0100

    PageContentInputTypeEvents js class

---
+++ Resources/public/javascript/page/PageContentInputTypeEvents.js
@@ -0,0 +1,40 @@
+/**
+ * Page content input type events class
+ *
+ * @author 	Alberto Vian - alberto@reaccionestudio.com
+ * @website reaccionestudio.com
+ */
+
+class PageContentInputTypeEvents
+{
+	/** 
+	 * Constructor
+	 */
+	constructor(mediaGalleryFormComponent) 
+	{
+		this.mediaGalleryFormComponent = mediaGalleryFormComponent;
+	}
+
+	/**
+	 * Events
+	 */
+	events()
+	{
+		let _self = this;
+
+		// 'Open gallery' button click event
+		$('[data-event="open-media-gallery"]').on("click", function(e)
+		{
+			// Get current type input value
+			let typeInput = $("select#page_content_type").val();
+			typeInput = (typeInput == "img") ? "image" : typeInput;
+
+			// Show media gallery modal box
+			_self.mediaGalleryFormComponent.showMediaGallery(typeInput);
+		});
+
+		
+	}
+}
+
+export default PageContentInputTypeEvents;
