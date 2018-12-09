f55c0831f50dcc227cc407f1d879da6f97b7f046
viher3 <viher_3@hotmail.com>
2018-12-09 16:33:13 +0100

    Added _setEditorContentOnFormSubmit() method in Entries.js.

--- Resources/public/javascript/entries/Entries.js
+++ Resources/public/javascript/entries/Entries.js
@@ -9,27 +9,50 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 class Entries
 {
+	/**
+	 * Entry form events
+	 */ 
 	formEvents()
 	{
+		this.editor = null;
 		this._initEditor("textarea#entry_content");
+		this._setEditorContentOnFormSubmit();
 	}
 
+	/**
+	 * Initialize CKeditor
+	 *
+	 * @param 	String 		editorId 	Textarea id attribute for CKeditor
+	 * @return 	void 		[type] 		
+	 */
 	_initEditor(editorId)
 	{
 		ClassicEditor
 		    .create( 
-		    	document.querySelector(editorId),
-		    	{
-		    		
-		    	}
+		    	document.querySelector(editorId), { }
 		    )
-		    .then( editor => {
-		        console.log( editor );
-		    } )
-		    .catch( error => {
+		    .then( editor => 
+		    {
+		    	this.editor = editor;
+		    })
+		    .catch( error => 
+		    {
 		        console.error( error );
-		    } );
+		    });
+	}
 
+	/**
+	 * Set editor content in textarea when form has been submitted
+	 */
+	_setEditorContentOnFormSubmit()
+	{
+		document.querySelector( '#submit' ).addEventListener( 'click', () => {
+			let ckeditorContent = this.editor.getData();
+			console.log(ckeditorContent);
+			$("textarea#entry_content").val(ckeditorContent);
+
+			return true;
+		});
 	}
 }
 
