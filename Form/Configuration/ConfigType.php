d6191fd0670aab15c0ab477787b2f965ec4cd704
viher3 <viher_3@hotmail.com>
2018-12-10 01:06:57 +0100

    Generate text inputs for serialized config parameter in ConfigType form

--- Form/Configuration/ConfigType.php
+++ Form/Configuration/ConfigType.php
@@ -15,6 +15,13 @@
 	{
 	    public function buildForm(FormBuilderInterface $builder, array $options)
 	    {
+	    	$valueInputClass = '';
+
+	    	if( ! empty($options['config']) && in_array($options['config']->getType(), ['image','serialized']) )
+	    	{
+	    		$valueInputClass = 'd-none';
+	    	}
+
 	    	// config entity fields
 	        $builder
 	            ->add('name', TextType::class, [
@@ -24,7 +31,8 @@
 	            ->add('value', TextareaType::class, [
 	            	'label' => 'config_form.value',
 	            	'required' => false,
-	            	'attr' => array('rows' => 8)
+	            	'attr' => array('rows' => 8, 'class' => $valueInputClass),
+	            	'label_attr' => array('class' => $valueInputClass)
 	            ])
 	            ->add('type', HiddenType::class, [
 	            	'required' => false
@@ -45,6 +53,21 @@
 	        			'attr' => ['class' => 'image-picker'],
 	        			'label_attr' => ['class' => 'image-picker-label']
 	        		]);
+	        	}
+	        	else if($type == "serialized")
+	        	{
+	        		$value = $options['config']->getValue();
+	        		$serializedData = unserialize($value);
+
+	        		foreach($serializedData as $key => $value)
+	        		{
+	        			$builder->add($key, TextType::class, [
+				            		  'label' => $key,
+				            		  'required' => false,
+				            		  'mapped' => false,
+				            		  'data' => $value
+				            	]);
+	        		}
 	        	}
 	        }
 	    }
