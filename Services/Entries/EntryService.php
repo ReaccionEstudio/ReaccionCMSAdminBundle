e95211fab75c8166a89806e394f0f0aff4b6d517
viher3 <viher_3@hotmail.com>
2018-12-15 18:32:28 +0100

    Created Entry service

---
+++ Services/Entries/EntryService.php
@@ -0,0 +1,61 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Services\Entries;
+
+	use Doctrine\ORM\EntityManagerInterface;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Entry;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Services\Config\ConfigService;
+
+	/**
+	 * Entry service.
+	 *
+	 * @author Alberto Vian <alberto@reaccionestudio.com>
+	 */
+	class EntryService
+	{
+		/**
+		 * @var ConfigService
+		 *
+		 * Config service
+		 */
+		private $config;
+
+		/**
+		 * Constructor
+		 */
+		public function __construct(ConfigService $config)
+		{
+			$this->config = $config;
+		}
+
+		/**
+		 * Generate entry resume
+		 *
+		 * 
+		 */
+		public function generateResume(Entry $entry) : Entry
+		{
+			// get words limit
+			$limit = $this->config->get("entries_resume_characters_length");
+			$limit = ( ! $limit || ! is_numeric($limit) || $limit < 0) ? 20 : $limit;
+			$content = $entry->getContent();
+
+			// clear content
+			$content = strip_tags($content);
+
+			// get resume
+			if(strlen($content) > $limit)
+			{
+				$resume = substr($content, 0, strrpos(substr($content, 0, $limit), " ")) . " ...";
+			}
+			else
+			{
+				$resume = $content;
+			}
+
+			// set resume to entry object
+			$entry->setResume($resume);
+
+			return $entry;
+		}
+	}
