e1bcde5eb9761b3e4a23293758e56b4e4ea6e4d2
viher3 <viher_3@hotmail.com>
2019-01-04 16:10:33 +0100

    Updated compile_assets sh script

--- Resources/bin/compile_assets.sh
+++ Resources/bin/compile_assets.sh
@@ -9,16 +9,20 @@ echo "Removing ${BUNDLES_TEMPLATES_PATH}/bundles folder ..."
 
 rm -R ${BUNDLES_TEMPLATES_PATH}/bundles
 
-# echo "Copying ${REACCION_ADMIN_BUNDLES_TEMPLATES_PATH} in ${BUNDLES_TEMPLATES_PATH}/bundles ..."
+echo "Copying ${REACCION_ADMIN_BUNDLES_TEMPLATES_PATH} in ${BUNDLES_TEMPLATES_PATH}/bundles ..."
 cp -R ${REACCION_ADMIN_BUNDLES_TEMPLATES_PATH} ${BUNDLES_TEMPLATES_PATH}/bundles
 
 # install assets
+echo "Installing assets ..."
 php ${SF_PATH}/bin/console assets:install --symlink 
 
 # Run Webpack encore
+echo "Compiling assets ..."
 yarn encore dev
 
 # Update translations
+echo "Updating translation files ..."
+rm -R ${SF_PATH}/translations/*.yml
 php ${SF_PATH}/bin/console translation:update en --force
 php ${SF_PATH}/bin/console translation:update es --force
 
