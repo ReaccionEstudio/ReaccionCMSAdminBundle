9228002a5d60374f6a7eae6d032fafe48fc001db
viher3 <viher_3@hotmail.com>
2019-01-06 23:41:54 +0100

    Mailer config type form

---
+++ Form/Configuration/MailerConfigType.php
@@ -0,0 +1,44 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Form\Configuration;
+
+	use Symfony\Component\Form\AbstractType;
+	use Symfony\Component\Form\FormBuilderInterface;
+	use Symfony\Component\Form\Extension\Core\Type\TextType;
+	use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+	use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+	class MailerConfigType extends AbstractType
+	{
+	    public function buildForm(FormBuilderInterface $builder, array $options)
+	    {
+	    	// config entity fields
+	        $builder
+	            ->add('host', TextType::class, [
+	            	'label' => 'preferences_mailer.host',
+	            	'required' => true
+	            ])
+	            ->add('port', TextType::class, [
+	            	'label' => 'preferences_mailer.port',
+	            	'required' => true
+	            ])
+	            ->add('username', TextType::class, [
+	            	'label' => 'preferences_mailer.username',
+	            	'required' => true
+	            ])
+	            ->add('password', PasswordType::class, [
+	            	'label' => 'preferences_mailer.password',
+	            	'required' => true
+	            ])
+	            ->add('authentication', ChoiceType::class, [
+	            	'label' => 'preferences_mailer.authentication',
+	            	'choices' => [
+	            		'SSL' => 'ssl',
+	            		'TLS' => 'tls'
+	            	],
+	            	'placeholder' => 'preferences_mailer.none',
+	            	'required' => false
+	            ])
+	        ;
+	    }
+ 	}
