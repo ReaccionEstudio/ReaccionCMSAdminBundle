eb9b522fa3e8165a28dd9580fd352e19cb22104e
viher3 <viher_3@hotmail.com>
2019-01-08 01:27:32 +0100

    List email template ctrl and view

---
+++ Controller/EmailTemplates/EmailTemplateController.php
@@ -0,0 +1,33 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\EmailTemplates;
+
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\EmailTemplate;
+
+	class EmailTemplateController extends Controller
+	{
+		public function index(Request $request)
+		{
+			$em = $this->getDoctrine()->getManager();
+			$emailTemplates = $em->getRepository(EmailTemplate::class)->findBy(
+						array(),
+						array('id' => 'DESC')
+					);
+
+			// pagination
+			$paginator = $this->get('knp_paginator');
+		    $emailTemplates = $paginator->paginate(
+		        $emailTemplates,
+		        $request->query->getInt('page', 1),
+		        $this->getParameter("pagination_page_limit")
+		    );
+
+			return $this->render("@ReaccionCMSAdminBundle/emailTemplates/list.html.twig",
+				[
+					'emailTemplates' => $emailTemplates
+				]
+			);
+		}
+	}
