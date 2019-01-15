4c522e55ffd7d89ec55839598fcc3c0d6ac4a4ca
viher3 <viher_3@hotmail.com>
2019-01-15 17:53:21 +0100

    Added create new translation group feature

---
+++ Form/Pages/PageTranslationGroupType.php
@@ -0,0 +1,20 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Form\Pages;
+
+	use Symfony\Component\Form\AbstractType;
+	use Symfony\Component\Form\FormBuilderInterface;
+	use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+	class PageTranslationGroupType extends AbstractType
+	{
+	    public function buildForm(FormBuilderInterface $builder, array $options)
+	    {
+	        $builder
+	            ->add('name', TextType::class, [
+	            	'label' => 'page_translation_group_form.name',
+	            	'required' => false
+	            ])
+	        ;
+	    }
+	}
