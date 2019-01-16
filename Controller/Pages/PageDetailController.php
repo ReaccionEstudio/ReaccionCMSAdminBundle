e80f83239df9186df22ffcff91f5deb3bedbaa06
viher3 <viher_3@hotmail.com>
2019-01-17 00:44:39 +0100

    Fixed get page language form value in PageDetailController refreshPageCache method

--- Controller/Pages/PageDetailController.php
+++ Controller/Pages/PageDetailController.php
@@ -2,6 +2,7 @@
 
 	namespace App\ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Pages;
 
+	use Symfony\Component\Form\Form;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\Translation\TranslatorInterface;
 	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
@@ -81,13 +82,14 @@
 		 * @param  Array  $pageForm 	Page form data
 		 * @return void   [type]
 		 */
-		private function refreshPageCache(Array $pageForm) : void
+		private function refreshPageCache(Form $pageForm) : void
 		{
-			// TODO: update menu html value for cache (we have to check if this page is in an existing menu)
-
+			// TODO: update menu html value for cache
+		
 			// refresh main page cache
 			if($pageForm['mainPage']->getData() == true)
 			{
+				$language = $pageForm['language']->getData();
 				$this->get("reaccion_cms_admin.page_cache_service")->refreshMainPageCache($language);
 			}
 		}
