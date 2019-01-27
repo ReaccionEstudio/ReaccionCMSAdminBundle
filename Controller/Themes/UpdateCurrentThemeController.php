86d6863875808c2ba89ef04405ea0cf5fe5dddfd
viher3 <viher_3@hotmail.com>
2019-01-27 20:34:32 +0100

    Appearance - themes routes and ctrls

--- Controller/Themes/UpdateCurrentThemeController.php
+++ Controller/Themes/UpdateCurrentThemeController.php
@@ -8,14 +8,21 @@
 
 	class UpdateCurrentThemeController extends Controller
 	{
-		public function index(String $themeFolderName, TranslatorInterface $translator)
+		private $translator;
+		
+		public function __construct(TranslatorInterface $translator)
+		{
+			$this->translator = $translator;	
+		}
+
+		public function index(String $themeFolderName)
 		{
 			$result = $this->get("reaccion_cms_admin.theme")->updateCurrentTheme($themeFolderName);
 
 			// flash message
 			$flashName = ($result) ? "success" : "error";
 			$translationKey = 'themes.update_theme_' . $flashName . '_message';
-			$message = $translator->trans($translationKey);
+			$message = $this->translator->trans($translationKey);
 			$this->addFlash($flashName, $message);
 
 			return $this->redirectToRoute('reaccion_cms_admin_appearance_themes');
