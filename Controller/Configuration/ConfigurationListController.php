1a30526c894401fd48bc004f8e4e8f206c031fde
viher3 <viher_3@hotmail.com>
2022-11-10 20:10:55 +0100

    cp

--- Controller/Configuration/ConfigurationListController.php
+++ Controller/Configuration/ConfigurationListController.php
@@ -2,10 +2,10 @@
 
 namespace ReaccionEstudio\ReaccionCMSAdminBundle\Controller\Configuration;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use ReaccionEstudio\ReaccionCMSBundle\Entity\Configuration;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ConfigurationListController extends AbstractController
 {
